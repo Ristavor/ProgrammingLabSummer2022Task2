badc7d47c4ea6d4be6d523263013f3470a2561c3
shmelev2.kv <shmelev2.kv@edu.spbstu.ru>
2022-05-17 19:13:51 +0300

    Added well-worked console

--- src/main/java/du/duLauncher.java
+++ src/main/java/du/duLauncher.java
@@ -1,1 +1,42 @@
+package du;
 
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.File;
+import java.util.List;
+
+public class duLauncher {
+
+        @Option(name = "-h", metaVar = "format", usage = "Turn into more easy-reading format")
+        private boolean format;
+
+        @Option(name = "-c", metaVar = "sum", usage = "Find sum of sizes")
+        private boolean sum;
+
+        @Option(name = "--si", metaVar = "inBits", usage = "Output in Bits")
+        private boolean inBits;
+
+        @Argument(required = true, metaVar = "fileNames", usage = "Files for find size")
+        private List<String> fileNamess;
+
+        private void launch(String[] args) {
+            CmdLineParser parser = new CmdLineParser(this);
+            try {
+                parser.parseArgument(args);
+            } catch (CmdLineException e) {
+                System.err.println(e.getMessage());
+                System.err.println("java -jar split.jar [-d] [-l num|-c num|-n num] [-o ofile] file");
+                parser.printUsage(System.err);
+                return;
+            }
+
+            du duer = new du();
+            duer.filesSize(fileNamess, format, sum, inBits);
+        }
+        public static void main(String[] args) {
+            new duLauncher().launch(args);
+        }
+    }
